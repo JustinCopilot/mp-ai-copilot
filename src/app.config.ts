febf19813c21e3e614c271251978550409aac1a9
chenjingming <jingming.chen@clife.cn>
2024-06-16 16:09:52 +0800

    fix: 页面路径优化

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,3 +1,24 @@
+export const pluginPages = [
+  "pages/list/list",
+  "pages/chat/chat",
+  "pages/chat_extension/index",
+  "pages/synthesis/synthesis",
+  "pages/share_img/share_img",
+  "pages/img_list/img_list",
+  "pages/redirect/redirect",
+]
+
+export const eduBehaviorPages = [
+  "education/pages/my_archive/index",
+  "education/pages/jot_down_reference_detail/index",
+  "education/pages/data_reference_detail/index",
+  "education/pages/choose_child/index",
+  "education/pages/jot_down_detail/index",
+  "education/pages/archive_observation/index",
+  "education/pages/information_supplement/index",
+  "education/pages/observation_detail/index"
+]
+
 export default defineAppConfig({
   pages: [
     'pages/index/index'
@@ -13,19 +34,8 @@ export default defineAppConfig({
     {
       root: "plugin/",
       pages: [
-        "pages/list/list",
-        "pages/chat/chat",
-        "pages/chat_extension/index",
-
-        // @TODO: 行为观察抽离单独分包
-        "education/pages/my_archive/index",
-        "education/pages/jot_down_reference_detail/index",
-        "education/pages/data_reference_detail/index",
-        "education/pages/choose_child/index",
-        "education/pages/jot_down_detail/index",
-        "education/pages/archive_observation/index",
-        "education/pages/information_supplement/index",
-        "education/pages/observation_detail/index"
+        ...pluginPages,
+        ...eduBehaviorPages, // @TODO: 行为观察抽离单独分包
       ],
     },
   ],
