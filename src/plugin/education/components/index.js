9208aacd197ead6ecb10f5ca578f3e7ad18d9c1c
chenjingming <jingming.chen@clife.cn>
2024-06-16 17:00:40 +0800

    fix: 修复报错

--- src/plugin/education/components/index.js
+++ src/plugin/education/components/index.js
@@ -1,5 +1,5 @@
 export * from './ReferenceDetail';
-export * from './archive';
+// export * from './archive';
 export * from './datePicker';
 export * from './SelectLabelPicker';
 export * from './ChooseGradeModal';
